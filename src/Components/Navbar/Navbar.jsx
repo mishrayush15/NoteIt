89d9c11595cc41e8d8693b0bd3dd3f320f62ed2d
Aditi Patil <aditiprakashpatil7@gmail.com>
2024-07-26 18:24:19 +0530

    basic structure of folders added

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='nav_container'>
+        <div className="nav_contents">
+            <div className="logo">
+                <h3>NoteIt</h3>
+            </div>
+            <div className="options">
+                <ul>
+                    <li>Home</li>
+                    <li>Community</li>
+                    <li>Room</li>
+                </ul>
+            </div>
+            <div className="side_menu">
+                <div className="icons">
+
+                </div>
+                <div className="icons">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
