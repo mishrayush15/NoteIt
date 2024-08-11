0c26f6a71dbedbd9728af22ee1af5f5a3a8c4236
Aditi Patil <aditiprakashpatil7@gmail.com>
2024-08-11 15:11:31 +0530

    cursor feature added

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
-
 import React, { useState } from "react";
 import Navbar from './Components/Navbar/Navbar';
 import LandingPage from './Components/LandingPage/LandingPage';
 import MainPart from './Components/UploadFile/MainPart/MainPart';
 import Footer from './Components/Footer/Footer'
 import './index.css';
+import gsap from 'gsap';
 
 const App = () => {
   const [theme, setTheme] = useState("light");
 
+  const cursorHandler = (e) => {
+    gsap.to(".cursor", {
+      x: e.clientX,
+      y: e.clientY,
+      duration: 0.3,
+      ease: 'power3.out'
+    });
+  };
+
   return (
-    <div className={`main_app_container ${theme}`}>
+    <div className={`main_app_container ${theme}`} onMouseMove={cursorHandler}>
+      <div className="cursor"></div>
       <Navbar theme={theme} setTheme={setTheme} />
       <LandingPage />
       <MainPart />
-      <Footer/>
+      <Footer />
     </div>
   );
 };
 
 export default App;
-
