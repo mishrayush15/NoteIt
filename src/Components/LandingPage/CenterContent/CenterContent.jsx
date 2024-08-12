1451464a93a4f0fc9ce0ab0eeb10640609e7b99c
Ayush Mishra <ayushmishra780193@gmail.com>
2024-08-12 22:00:17 +0530

    bug fix

--- src/Components/LandingPage/CenterContent/CenterContent.jsx
+++ src/Components/LandingPage/CenterContent/CenterContent.jsx
@@ -81,7 +81,7 @@ const CenterContent = () => {
         </div>
         <div className="right_part">
           <div className="image">
-            <img src={"./public/assets/hero.png"} alt="" />
+            <img src={"/assets/hero.png"} alt="" />
           </div>
         </div>
       </div>
